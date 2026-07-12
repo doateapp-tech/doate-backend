37c25abd13be1056fb68c8893360856bbeb4c7c1
Victor Canhengue <doate.app@gmail.com>
2026-07-12 20:34:39 +0100

    migrar nodemailer para resend

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,20 +1,11 @@
 const bcrypt = require("bcryptjs");
 const db = require("../config/db");
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-});
+const { enviarCodigo } = require("../services/emailService");
 
 function gerarCodigo() {
     return Math.floor(100000 + Math.random() * 900000).toString();
 }
+
 
 exports.register = async(req, res) => {
     try {
@@ -44,16 +35,7 @@ exports.register = async(req, res) => {
             `, [nome, email, telefone, senhaHash, codigo, expiraEm]
         );
 
-        await transporter.sendMail({
-            from: `"Doate App" <${process.env.EMAIL_USER}>`,
-            to: email,
-            subject: "Código de verificação",
-            html: `
-                <h2>Verificação de Email</h2>
-                <h1 style="letter-spacing:6px">${codigo}</h1>
-                <p>Expira em 5 minutos</p>
-            `,
-        });
+        await enviarCodigo(email, codigo);
 
         return res.status(201).json({
             message: "Usuário criado. Verifique seu email.",
@@ -134,17 +116,8 @@ exports.resendCode = async(req, res) => {
             WHERE email = ?
             `, [codigo, expiraEm, email]
         );
+        await enviarCodigo(email, codigo);
 
-        await transporter.sendMail({
-            from: `"Doate App" <${process.env.EMAIL_USER}>`,
-            to: email,
-            subject: "Novo código de verificação",
-            html: `
-                <h2>Novo código</h2>
-                <h1 style="letter-spacing:6px">${codigo}</h1>
-                <p>Expira em 5 minutos</p>
-            `,
-        });
 
         return res.json({ message: "Novo código enviado" });
 
